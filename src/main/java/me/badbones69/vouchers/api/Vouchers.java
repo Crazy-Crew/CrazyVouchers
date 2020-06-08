3ceafc56c618fadd53435c761fd6ab999d956c79
BadBones69 <joewojcik14@gmail.com>
2020-06-08 00:15:18 -0400

    Removed old voucher item check.
    Removed:
    - Removed checking item's material, name, and lore to see if they are the same. Now only checks if item contains the correct NBT tag. This was kept in the plugin to support old voucher items that didn't have them. It is now removed as NBT tags have been in the plugin for a long time.

--- src/main/java/me/badbones69/vouchers/api/Vouchers.java
+++ src/main/java/me/badbones69/vouchers/api/Vouchers.java
@@ -1,6 +1,5 @@
 package me.badbones69.vouchers.api;
 
-import me.badbones69.vouchers.Methods;
 import me.badbones69.vouchers.api.FileManager.Files;
 import me.badbones69.vouchers.api.itemnbtapi.NBTItem;
 import me.badbones69.vouchers.api.objects.Voucher;
@@ -10,7 +9,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Vouchers {
@@ -93,38 +91,6 @@ public class Vouchers {
             }
         } catch (Exception e) {
         }
-        try {
-            if (item.hasItemMeta()) {
-                if (item.getItemMeta().hasDisplayName() && item.getItemMeta().hasLore()) {
-                    for (Voucher voucher : getVouchers()) {
-                        if (voucher.usesArguments()) {
-                            String argument = getArgument(item, voucher);
-                            if (argument != null) {
-                                if (Methods.isSimilar(item, voucher.buildItem(argument))) {
-                                    return voucher;
-                                }
-                            }
-                        } else if (item.getItemMeta().getDisplayName().equals(voucher.buildItem().getItemMeta().getDisplayName())) {
-                            int line = 0;
-                            boolean sameLore = true;
-                            ItemStack voucherItem = voucher.buildItem();
-                            for (String lore : item.getItemMeta().getLore()) {
-                                if (!lore.equals(voucherItem.getItemMeta().getLore().get(line))) {
-                                    sameLore = false;
-                                    break;
-                                }
-                                line++;
-                            }
-                            if (sameLore) {
-                                return voucher;
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return null;
     }
     
@@ -135,40 +101,6 @@ public class Vouchers {
             if (nbt.hasKey("voucher") && nbt.hasKey("argument")) {
                 if (nbt.getString("voucher").equalsIgnoreCase(voucher.getName())) {
                     return nbt.getString("argument");
-                }
-            }
-            //Using the old method to check for old vouchers or vouchers given without nbt tags.
-            String itemName = item.getItemMeta().getDisplayName();
-            List<String> itemLore = item.getItemMeta().getLore();
-            String voucherName = voucher.buildItem("%arg%").getItemMeta().getDisplayName();
-            List<String> voucherLore = voucher.buildItem().getItemMeta().getLore();
-            String argument = "";
-            int line = 0;
-            if (voucherName.contains("%arg%")) {
-                String[] b = voucherName.split("%arg%");
-                if (b.length >= 1) argument = itemName.replace(b[0], "");
-                if (b.length >= 2) argument = argument.replace(b[1], "");
-                if (itemName.equalsIgnoreCase(voucher.buildItem(argument).getItemMeta().getDisplayName())) {
-                    return argument;
-                }
-            }
-            if (itemLore.size() == voucherLore.size()) {
-                for (String lore : voucherLore) {
-                    String itemLine = itemLore.get(line);
-                    if (lore.contains("%arg%")) {
-                        String[] b = lore.split("%arg%");
-                        if (!itemLine.startsWith(b[0])) {
-                            break;
-                        }
-                        argument = itemLine.replace(b[0], "");
-                        if (b.length >= 2) argument = argument.replace(b[1], "");
-                        if (Methods.isSimilar(item, voucher.buildItem(argument))) {
-                            return argument;
-                        }
-                    } else if (!itemLore.get(line).equals(lore)) {
-                        break;
-                    }
-                    line++;
                 }
             }
         }
