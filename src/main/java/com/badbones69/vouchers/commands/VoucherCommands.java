f6db28427fe8078efa74fb8e174f1e3774053ab3
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-08-06 14:20:33 -0400

    Use enhanced switch

--- src/main/java/com/badbones69/vouchers/commands/VoucherCommands.java
+++ src/main/java/com/badbones69/vouchers/commands/VoucherCommands.java
@@ -33,14 +33,13 @@ public class VoucherCommands implements CommandExecutor {
             return true;
         } else {
             switch (args[0].toLowerCase()) {
-                case "help":
-
+                case "help" -> {
                     if (Methods.hasPermission(sender, "access")) {
                         sender.sendMessage(Messages.HELP.getMessageNoPrefix());
                     }
-
                     return true;
-                case "reload":
+                }
+                case "reload" -> {
                     if (Methods.hasPermission(sender, "admin")) {
                         FileManager.getInstance().reloadAllFiles();
                         fileManager.setup(crazyManager.getPlugin());
@@ -53,10 +52,9 @@ public class VoucherCommands implements CommandExecutor {
                         crazyManager.load();
                         sender.sendMessage(Messages.RELOAD.getMessage());
                     }
-
                     return true;
-                case "open":
-                case "admin":
+                }
+                case "open", "admin" -> {
                     if (Methods.hasPermission(sender, "admin")) {
                         int page = 1;
 
@@ -66,10 +64,9 @@ public class VoucherCommands implements CommandExecutor {
 
                         GUI.openGUI((Player) sender, page);
                     }
-
                     return true;
-                case "types":
-                case "list":
+                }
+                case "types", "list" -> {
                     if (Methods.hasPermission(sender, "admin")) {
                         StringBuilder vouchers = new StringBuilder();
                         StringBuilder codes = new StringBuilder();
@@ -87,9 +84,9 @@ public class VoucherCommands implements CommandExecutor {
                         sender.sendMessage(Methods.color("&e&lVouchers #" + CrazyManager.getVouchers().size() + ":&f " + vouchers));
                         sender.sendMessage(Methods.color("&e&lVoucher Codes #" + CrazyManager.getVoucherCodes().size() + ":&f " + codes));
                     }
-
                     return true;
-                case "redeem":
+                }
+                case "redeem" -> {
                     if (Methods.hasPermission(sender, "redeem")) {
                         if (args.length >= 2) {
                             String code = args[1];
@@ -241,7 +238,8 @@ public class VoucherCommands implements CommandExecutor {
                         sender.sendMessage(Methods.getPrefix("&c/voucher redeem <code>"));
                     }
                     return true;
-                case "give": // /Voucher 0Give 1<Type> 2[Amount] 3[Player] 4[Arguments]
+                }
+                case "give" -> { // /Voucher 0Give 1<Type> 2[Amount] 3[Player] 4[Arguments]
                     if (Methods.hasPermission(sender, "admin")) {
 
                         if (args.length > 1) {
@@ -287,7 +285,8 @@ public class VoucherCommands implements CommandExecutor {
                             placeholders.put("%Player%", player.getName());
                             placeholders.put("%Voucher%", voucher.getName());
 
-                            if (!Messages.GIVEN_A_VOUCHER.isBlank()) sender.sendMessage(Messages.GIVEN_A_VOUCHER.getMessage(placeholders));
+                            if (!Messages.GIVEN_A_VOUCHER.isBlank())
+                                sender.sendMessage(Messages.GIVEN_A_VOUCHER.getMessage(placeholders));
 
                             return true;
                         }
@@ -296,7 +295,8 @@ public class VoucherCommands implements CommandExecutor {
                         return true;
                     }
                     return true;
-                case "giveall": // /Voucher 0GiveAll 1<Type> 2[Amount] 3[Arguments]
+                }
+                case "giveall" -> { // /Voucher 0GiveAll 1<Type> 2[Amount] 3[Arguments]
                     if (Methods.hasPermission(sender, "admin")) {
 
                         if (args.length > 1) {
@@ -343,9 +343,11 @@ public class VoucherCommands implements CommandExecutor {
                         return true;
                     }
                     return true;
-                default:
+                }
+                default -> {
                     sender.sendMessage(Methods.getPrefix("&cPlease do /voucher help for more information."));
                     return true;
+                }
             }
         }
     }
