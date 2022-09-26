95a69e2d0c1418f03720adf8f461ee13e77ecddd
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-09-26 18:26:07 -0400

    Use PDC

--- src/main/java/com/badbones69/vouchers/controllers/FireworkDamageAPI.java
+++ src/main/java/com/badbones69/vouchers/controllers/FireworkDamageAPI.java
@@ -2,28 +2,38 @@ package com.badbones69.vouchers.controllers;
 
 import com.badbones69.vouchers.Vouchers;
 import com.badbones69.vouchers.api.CrazyManager;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Firework;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 public class FireworkDamageAPI implements Listener {
 
     public final Vouchers plugin = Vouchers.getPlugin();
-    
+
+    NamespacedKey noDamage = new NamespacedKey(plugin, "no-damage");
+
     /**
      * @param firework The firework you want to add.
      */
     public void addFirework(Entity firework) {
-        firework.setMetadata("nodamage", new FixedMetadataValue(plugin, true));
+        PersistentDataContainer container = firework.getPersistentDataContainer();
+
+        container.set(noDamage, PersistentDataType.STRING, "no-damage");
     }
 
     @EventHandler
     public void onPlayerDamage(EntityDamageByEntityEvent e) {
-        if (e.getDamager() instanceof Firework fw) {
-            if (fw.hasMetadata("nodamage")) e.setCancelled(true);
+        if (e.getDamager() instanceof Firework firework) {
+
+            PersistentDataContainer container = firework.getPersistentDataContainer();
+
+            if (container.has(noDamage, PersistentDataType.STRING)) e.setCancelled(true);
         }
     }
 }
