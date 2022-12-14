3c59d62bbb63f6459130511e16fc093756e8c418
Ryder Belserion <no-reply@ryderbelserion.com>
2022-12-13 22:45:01 -0500

    Update build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
-    java
+    `java-library`
+
+    `maven-publish`
+
+    id("com.modrinth.minotaur") version "2.5.0"
 
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-val buildNumber: String? = System.getenv("BUILD_NUMBER")
-
-val jenkinsVersion = "2.9.13-b$buildNumber"
-
-group = "com.badbones69.vouchers"
-version = "2.9.13"
-description = "Want to make a paper that can give you an axolotl with a pretty firework display, Look no further! "
+project.group = "com.badbones69.crazyvouchers"
+project.version = "${extra["plugin_version"]}"
+project.description = "Want to make a paper that can give you an axolotl with a pretty firework display, Look no further! "
 
 repositories {
     /**
@@ -51,13 +51,19 @@ java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
+// val buildNumber: String? = System.getenv("BUILD_NUMBER")
+// val buildVersion = "${project.version}-b$buildNumber"
+
 tasks {
     shadowJar {
-        if (buildNumber != null) {
-            archiveFileName.set("${rootProject.name}-[v${jenkinsVersion}].jar")
-        } else {
-            archiveFileName.set("${rootProject.name}-[v${rootProject.version}].jar")
-        }
+        /**
+         * Uncomment this when in the Dev Branch but make sure to comment it before making a pr to the main branch.
+         */
+        //if (buildNumber != null) {
+        //    archiveFileName.set("${rootProject.name}-${buildVersion}.jar")
+        //} else {
+        //    archiveFileName.set("${rootProject.name}-${project.version}.jar")
+        //}
 
         listOf(
             "de.tr7zw",
@@ -65,6 +71,22 @@ tasks {
         ).forEach {
             relocate(it, "${rootProject.group}.plugin.lib.$it")
         }
+    }
+
+    modrinth {
+        token.set(System.getenv("MODRINTH_TOKEN"))
+        projectId.set("crazyvouchers")
+        versionName.set("${rootProject.name} ${project.version} Update")
+        versionNumber.set("${project.version}")
+        versionType.set("${extra["version_type"]}")
+        uploadFile.set(shadowJar.get())
+
+        autoAddDependsOn.set(true)
+
+        gameVersions.addAll(listOf("1.19", "1.19.1", "1.19.2", "1.19.3"))
+        loaders.addAll(listOf("paper", "purpur"))
+
+        changelog.set(System.getenv("COMMIT_MESSAGE"))
     }
 
     compileJava {
@@ -75,10 +97,31 @@ tasks {
         filesMatching("plugin.yml") {
             expand(
                 "name" to rootProject.name,
-                "group" to rootProject.group,
-                "version" to rootProject.version,
-                "description" to rootProject.description
+                "group" to project.group,
+                "version" to project.version,
+                "description" to project.description
             )
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven("https://repo.crazycrew.us/releases") {
+            name = "crazycrew"
+            credentials {
+                username = System.getenv("CRAZYCREW_USERNAME")
+                password = System.getenv("CRAZYCREW_PASSWORD")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "${project.group}"
+            artifactId = rootProject.name.toLowerCase()
+            version = "${project.version}"
+            from(components["java"])
         }
     }
 }
