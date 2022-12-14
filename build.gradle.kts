f38ee5c402304d00d37d0e683ea4c79c8876524e
Ryder Belserion <no-reply@ryderbelserion.com>
2022-12-13 23:08:42 -0500

    Use correct env variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,8 +107,8 @@ publishing {
         maven("https://repo.crazycrew.us/releases") {
             name = "crazycrew"
             credentials {
-                username = System.getenv("CRAZYCREW_USERNAME")
-                password = System.getenv("CRAZYCREW_PASSWORD")
+                username = System.getenv("REPOSITORY_USERNAME")
+                password = System.getenv("REPOSITORY_PASSWORD")
             }
         }
     }
