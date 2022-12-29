145ed813430faa56d85b966678b76e70a6512de5
Ryder Belserion <no-reply@ryderbelserion.com>
2022-12-29 09:58:41 -0500

    Don't run if data folder already exists
    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,7 +109,7 @@ tasks {
 
 publishing {
     repositories {
-        maven("https://repo.crazycrew.us/snapshots") {
+        maven("https://repo.crazycrew.us/releases") {
             name = "crazycrew"
             credentials {
                 username = System.getenv("REPOSITORY_USERNAME")
