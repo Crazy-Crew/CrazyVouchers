a2213e68e17b6407e30140d7d9480a65f76272ed
Ryder Belserion <no-reply@ryderbelserion.com>
2023-09-22 12:06:40 -0400

    Update changelog

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -83,12 +83,12 @@ val file = file("${rootProject.rootDir}/jars/${rootProject.name}-${rootProject.v
 val description = """
 ## Changes:
 * Added 1.20.2 support.
-* Moved all vouchers out of Config.yml into the vouchers folder. It will automatically convert
 * Made sounds respect client side sound settings.
 * Properly handle how Metrics shuts down and turns on when you change the true/false.
 * Add more verbose logging with an option to turn off the spammy garbage.
 * Internal placeholders like %random% have been changed to {random}, All internal usage's of the placeholders have been changed. I cannot change that for where you use the commands.
- * I do however still take those as valid input, I simply convert %random% to {random} under the hood but please change it. I won't be doing that forever!
+ * I do however still take those as valid input since I simply convert %random% to {random} under the hood but please change it, I won't be doing that forever though...
+* Added volume/pitch controls to each sound section in the vouchers.
 
 ## Breaking Changes:
 * Vouchers have been moved out of the Config.yml into a folder called `vouchers`
