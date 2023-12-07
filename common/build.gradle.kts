e779e9eae3528354d365ea99be5c9b81c775b6fd
Ryder Belserion <no-reply@ryderbelserion.com>
2023-12-07 11:13:10 -0500

    it boots!

---
+++ common/build.gradle.kts
@@ -0,0 +1,13 @@
+project.group = "${rootProject.group}.common"
+
+base {
+    archivesName.set("${rootProject.name}-${project.name}")
+}
+
+dependencies {
+    api(libs.config.me) {
+        exclude(group = "org.yaml", module = "snakeyaml")
+    }
+
+    compileOnly(libs.cluster.api)
+}
