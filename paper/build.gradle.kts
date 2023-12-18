488ab113aeb0c97169f96b741d08ee4132d1a51f
Ryder Belserion <no-reply@ryderbelserion.com>
2023-12-18 01:12:38 -0500

    [ci-skip] Update assemble task

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -45,8 +45,8 @@ val isBeta: Boolean get() = rootProject.extra["isBeta"]?.toString()?.toBoolean()
 val type = if (isBeta) "Beta" else "Release"
 
 val description = """
-## Changes:
- * N/A
+## Fixes:
+ * Fix an NPE with item builder
 
 ## Other:
  * [Feature Requests](https://github.com/Crazy-Crew/${rootProject.name}/issues)
