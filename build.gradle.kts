b8f72a939e65bc2b0d6694cee8fdcbda10e6f920
Ryder Belserion <no-reply@ryderbelserion.com>
2023-12-19 14:53:32 -0500

    no likey

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,59 +23,59 @@ tasks {
                     }
                 }.onSuccess {
                     // Delete to save space on jenkins.
-                    //delete(project.layout.buildDirectory.get())
-                    //delete(rootProject.layout.buildDirectory.get())
+                    delete(project.layout.buildDirectory.get())
+                    delete(rootProject.layout.buildDirectory.get())
                 }.onFailure {
                     println("Failed to copy file out of build folder into jars directory: Likely does not exist.")
                 }
             }
         }
     }
+}
 
-    subprojects {
-        apply(plugin = "java-library")
+subprojects {
+    apply(plugin = "java-library")
+
+    repositories {
+        maven("https://repo.crazycrew.us/releases")
+
+        maven("https://jitpack.io/")
+
+        mavenCentral()
+    }
+
+    if (name == "paper") {
+        project.version = if (System.getenv("BUILD_NUMBER") != null) "${rootProject.version}-${System.getenv("BUILD_NUMBER")}" else rootProject.version
 
         repositories {
-            maven("https://repo.crazycrew.us/releases")
+            maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 
-            maven("https://jitpack.io/")
+            maven("https://repo.codemc.io/repository/maven-public/")
 
-            mavenCentral()
+            maven("https://repo.triumphteam.dev/snapshots/")
+
+            maven("https://repo.oraxen.com/releases/")
+
+            flatDir { dirs("libs") }
+        }
+    }
+
+    tasks {
+        compileJava {
+            options.encoding = Charsets.UTF_8.name()
+            options.release.set(17)
         }
 
-        if (name == "paper") {
-            project.version = if (System.getenv("BUILD_NUMBER") != null) "${rootProject.version}-${System.getenv("BUILD_NUMBER")}" else rootProject.version
-
-            repositories {
-                maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-
-                maven("https://repo.codemc.io/repository/maven-public/")
-
-                maven("https://repo.triumphteam.dev/snapshots/")
-
-                maven("https://repo.oraxen.com/releases/")
-
-                flatDir { dirs("libs") }
-            }
+        javadoc {
+            options.encoding = Charsets.UTF_8.name()
         }
 
-        tasks {
-            compileJava {
-                options.encoding = Charsets.UTF_8.name()
-                options.release.set(17)
-            }
-
-            javadoc {
-                options.encoding = Charsets.UTF_8.name()
-            }
-
-            processResources {
-                filteringCharset = Charsets.UTF_8.name()
-            }
+        processResources {
+            filteringCharset = Charsets.UTF_8.name()
         }
+    }
 
-        java {
-            toolchain.languageVersion.set(JavaLanguageVersion.of("17"))
-        }
+    java {
+        toolchain.languageVersion.set(JavaLanguageVersion.of("17"))
     }
 }
