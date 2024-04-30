5eab0b16fe2f46a68a31d770320892ebc237a21d
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-30 13:35:56 -0400

    Update gradle files

--- buildSrc/src/main/kotlin/root-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/root-plugin.gradle.kts
@@ -29,7 +29,9 @@ repositories {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of("21"))
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 tasks {
