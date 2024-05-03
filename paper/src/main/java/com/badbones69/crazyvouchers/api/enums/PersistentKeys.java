5af33df743a320a04f947c12527d74d2f6c758df
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-03 02:32:09 -0400

    Remove type from enum

--- paper/src/main/java/com/badbones69/crazyvouchers/api/enums/PersistentKeys.java
+++ paper/src/main/java/com/badbones69/crazyvouchers/api/enums/PersistentKeys.java
@@ -2,34 +2,27 @@ package com.badbones69.crazyvouchers.api.enums;
 
 import com.badbones69.crazyvouchers.CrazyVouchers;
 import org.bukkit.NamespacedKey;
-import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public enum PersistentKeys {
 
-    voucher_item_admin("voucher_item_admin", PersistentDataType.STRING),
-    voucher_item("voucher_item", PersistentDataType.STRING),
-    voucher_argument("voucher_argument", PersistentDataType.STRING),
-    back_button("back_button", PersistentDataType.STRING),
-    next_button("next_button", PersistentDataType.STRING),
-    no_firework_damage("firework", PersistentDataType.BOOLEAN);
+    voucher_item_admin("voucher_item_admin"), // string
+    voucher_item("voucher_item"), // string
+    voucher_argument("voucher_argument"), // string
+    back_button("back_button"), // string
+    next_button("next_button"), // string
+    no_firework_damage("firework"); // boolean
 
     private final @NotNull CrazyVouchers plugin = JavaPlugin.getPlugin(CrazyVouchers.class);
 
     private final String NamespacedKey;
-    private final PersistentDataType type;
 
-    PersistentKeys(String NamespacedKey, PersistentDataType type) {
+    PersistentKeys(String NamespacedKey) {
         this.NamespacedKey = NamespacedKey;
-        this.type = type;
     }
 
     public NamespacedKey getNamespacedKey() {
         return new NamespacedKey(this.plugin, this.plugin.getName().toLowerCase() + "_" + this.NamespacedKey);
-    }
-
-    public PersistentDataType getType() {
-        return this.type;
     }
 }
