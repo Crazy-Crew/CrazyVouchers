64161e7f5b93f0a0e06aec00112873e7400848a6
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-04 02:49:20 -0400

    remove raw usage of pdc types

--- paper/src/main/java/com/badbones69/crazyvouchers/api/enums/PersistentKeys.java
+++ paper/src/main/java/com/badbones69/crazyvouchers/api/enums/PersistentKeys.java
@@ -7,26 +7,20 @@ import org.jetbrains.annotations.NotNull;
 
 public enum PersistentKeys {
 
-    voucher_item_admin("voucher_item_admin", PersistentDataType.STRING),
-    back_button("back_button", PersistentDataType.STRING),
-    next_button("next_button", PersistentDataType.STRING),
-    no_firework_damage("firework", PersistentDataType.BOOLEAN);
+    voucher_item_admin("voucher_item_admin"),
+    back_button("back_button"),
+    next_button("next_button"),
+    no_firework_damage("firework");
 
     private final @NotNull CrazyVouchers plugin = CrazyVouchers.get();
 
     private final String NamespacedKey;
-    private final PersistentDataType type;
 
-    PersistentKeys(String NamespacedKey, PersistentDataType type) {
+    PersistentKeys(String NamespacedKey) {
         this.NamespacedKey = NamespacedKey;
-        this.type = type;
     }
 
     public NamespacedKey getNamespacedKey() {
         return new NamespacedKey(this.plugin, this.plugin.getName().toLowerCase() + "_" + this.NamespacedKey);
-    }
-
-    public PersistentDataType getType() {
-        return this.type;
     }
 }
