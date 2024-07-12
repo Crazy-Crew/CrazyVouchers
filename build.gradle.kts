93589e8d6622c4479839df969a95ad033ed0a449
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-11 22:17:08 -0400

    [ci-skip] update release channel for hangar

--- build.gradle.kts
+++ build.gradle.kts
@@ -138,7 +138,7 @@ tasks {
 
             version.set(rootProject.version as String)
 
-            channel.set(if (isSnapshot) "Snapshot" else "Release")
+            channel.set(if (isSnapshot) "Beta" else "Release")
 
             changelog.set(content)
 
