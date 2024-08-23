533c071b7f3252760084b6ff678a1bb65274349e
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-23 11:55:17 -0400

    add new toggle to metrics

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
-### Changes:
-- Bumped to 1.21.1
-- Bumped nbt api
+### Additions:
+- Added a toggle for dupe protection which assigns uuids to new vouchers
+  - Once a voucher is used, the uuid gets thrown in the data.yml
+  - If a voucher is used with the same uuid again. it will tell the player no and notify all staff
+  - Previous vouchers already given, won't be accounted with this, so you'll just have to phase those out.
+- Added a new permission, crazyvouchers.notify.duped which notifies any player who has the permission of duped vouchers
--- src/main/java/com/badbones69/crazyvouchers/support/MetricsWrapper.java
+++ src/main/java/com/badbones69/crazyvouchers/support/MetricsWrapper.java
@@ -1,5 +1,6 @@
 package com.badbones69.crazyvouchers.support;
 
+import ch.jalu.configme.SettingsManager;
 import com.badbones69.crazyvouchers.CrazyVouchers;
 import com.badbones69.crazyvouchers.config.ConfigManager;
 import com.badbones69.crazyvouchers.config.types.ConfigKeys;
@@ -16,10 +17,13 @@ public class MetricsWrapper extends bStats {
         super(plugin, serviceId);
     }
 
+    private final SettingsManager config = ConfigManager.getConfig();
+
     public void start() {
         // If it's not enabled, we do nothing!
         if (!isEnabled()) return;
 
-        addCustomChart(new SimplePie("old_file_system", () -> String.valueOf(ConfigManager.getConfig().getProperty(ConfigKeys.mono_file).booleanValue())));
+        addCustomChart(new SimplePie("old_file_system", () -> String.valueOf(this.config.getProperty(ConfigKeys.mono_file).booleanValue())));
+        addCustomChart(new SimplePie("use_dupe_protection", () -> String.valueOf(this.config.getProperty(ConfigKeys.dupe_protection).booleanValue())));
     }
 }
