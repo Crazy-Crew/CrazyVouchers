d95e6730617b02ad7f5241b733c7c3c7646e1779
Ryder Belserion <no-reply@ryderbelserion.com>
2024-09-02 22:55:09 -0400

    update buildscript

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,1 @@
 rootProject.name = "CrazyVouchers"
-
-pluginManagement {
-    repositories {
-        maven("https://repo.crazycrew.us/releases")
-
-        gradlePluginPortal()
-    }
-}
-
-plugins {
-    id("com.ryderbelserion.feather-settings") version "0.0.4"
-}
