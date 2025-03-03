b3652959d5c6e0269c8aaab0b1a77e1cbf2d85a3
Ryder Belserion <contact@ryderbelserion.com>
2025-03-02 22:18:20 -0500

    Add extra variables

--- paper/src/main/java/com/badbones69/crazyvouchers/commands/BaseCommand.java
+++ paper/src/main/java/com/badbones69/crazyvouchers/commands/BaseCommand.java
@@ -1,8 +1,10 @@
 package com.badbones69.crazyvouchers.commands;
 
+import ch.jalu.configme.SettingsManager;
 import com.badbones69.crazyvouchers.CrazyVouchers;
 import com.badbones69.crazyvouchers.api.CrazyManager;
 import com.badbones69.crazyvouchers.api.InventoryManager;
+import com.badbones69.crazyvouchers.config.ConfigManager;
 import com.ryderbelserion.fusion.paper.files.FileManager;
 import dev.triumphteam.cmd.core.annotations.Command;
 
@@ -16,5 +18,7 @@ public abstract class BaseCommand {
     protected final CrazyManager crazyManager = this.plugin.getCrazyManager();
 
     protected final InventoryManager inventoryManager = this.plugin.getInventoryManager();
+
+    protected final SettingsManager config = ConfigManager.getConfig();
 
 }
