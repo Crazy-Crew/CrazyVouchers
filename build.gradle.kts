a6bc6cb3ea1e5c7c950d1678981afc85b2f2e763
Ryder Belserion <contact@ryderbelserion.com>
2025-04-15 22:18:54 -0400

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,6 @@ rootProject.group = "com.badbones69.crazyvouchers"
 rootProject.description = "Give your players as many rewards as you like in a compact form called a voucher!"
 
 val buildNumber: String? = System.getenv("BUILD_NUMBER")
+val isPublishing: String? = System.getenv("IS_PUBLISHING")
 
-rootProject.version = if (buildNumber != null) "${libs.versions.minecraft.get()}-$buildNumber" else "4.1.1"
+rootProject.version = if (buildNumber != null && isPublishing == null) "${libs.versions.minecraft.get()}-$buildNumber" else rootProject.properties["version"].toString()
