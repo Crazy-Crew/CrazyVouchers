a56090b1d3eff4ee9b36715655756495919d48d6
Ryder Belserion <contact@ryderbelserion.com>
2025-07-14 15:32:01 -0400

    Update build script

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
-    alias(libs.plugins.runPaper)
-    alias(libs.plugins.shadow)
-
-    id("paper-plugin")
+    `config-paper`
 }
 
 project.version = rootProject.version
@@ -29,9 +26,21 @@ dependencies {
     compileOnly(libs.bundles.shared)
 }
 
-val component: SoftwareComponent = components["java"]
-
 tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    shadowJar {
+        listOf(
+            "org.bstats"
+        ).forEach {
+            relocate(it, "libs.$it")
+        }
+    }
+
+    runPaper.folia.registerTask()
+
     runServer {
         jvmArgs("-Dnet.kyori.ansi.colorLevel=truecolor")
         jvmArgs("-Dcom.mojang.eula.agree=true")
@@ -39,66 +48,5 @@ tasks {
         defaultCharacterEncoding = Charsets.UTF_8.name()
 
         minecraftVersion(libs.versions.minecraft.get())
-    }
-
-    assemble {
-        dependsOn(shadowJar)
-
-        doLast {
-            copy {
-                from(shadowJar.get())
-                into(rootProject.projectDir.resolve("jars"))
-            }
-        }
-    }
-
-    shadowJar {
-        archiveBaseName.set(rootProject.name)
-        archiveClassifier.set("")
-
-        listOf(
-            "com.ryderbelserion.fusion",
-            "de.tr7zw.changeme.nbtapi",
-            "org.bstats"
-        ).forEach {
-            relocate(it, "libs.$it")
-        }
-    }
-
-    processResources {
-        inputs.properties("name" to rootProject.name)
-        inputs.properties("version" to project.version)
-        inputs.properties("group" to rootProject.group)
-        inputs.properties("authors" to rootProject.properties["authors"].toString())
-        inputs.properties("apiVersion" to libs.versions.minecraft.get())
-        inputs.properties("description" to rootProject.description)
-        inputs.properties("website" to rootProject.properties["website"].toString())
-
-        filesMatching("paper-plugin.yml") {
-            expand(inputs.properties)
-        }
-    }
-
-    publishing {
-        repositories {
-            maven {
-                url = uri("https://repo.crazycrew.us/releases")
-
-                credentials {
-                    this.username = System.getenv("gradle_username")
-                    this.password = System.getenv("gradle_password")
-                }
-            }
-        }
-
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = rootProject.group.toString()
-                artifactId = "${rootProject.name.lowercase()}-${project.name.lowercase()}-api"
-                version = rootProject.version.toString()
-
-                from(component)
-            }
-        }
     }
 }
